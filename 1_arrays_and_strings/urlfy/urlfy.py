4edce9f37262d53d1613f5ded011d58d674ce4c1
rubenhortas <rubenhortas@gmail.com>
2024-05-08 18:19:08 +0200

    Refactored

--- 1_arrays_and_strings/urlfy/urlfy.py
+++ 1_arrays_and_strings/urlfy/urlfy.py
@@ -3,13 +3,13 @@ def get_url(string: list, true_length: int) -> list:
     # Auxiliary space: O(1)
     spaces = 0
 
-    for i in range(true_length - 1, 0, -1):
+    for i in reversed(range(true_length)):
         if string[i] == ' ':
             spaces = spaces + 1
 
     index = (true_length + (2 * spaces)) - 1
 
-    for i in range(true_length - 1, 0, -1):  # (start, stop, step)
+    for i in reversed(range(true_length)):
         if string[i] != ' ':
             string[index] = string[i]
             index = index - 1
