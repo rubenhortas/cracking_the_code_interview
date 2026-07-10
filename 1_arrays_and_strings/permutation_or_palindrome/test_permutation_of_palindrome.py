28a83ee633d5072f9b5fd919b862e68942f1ead0
rubenhortas <rubenhortas@gmail.com>
2026-07-10 11:13:13 +0200

    style: ruff

--- 1_arrays_and_strings/permutation_or_palindrome/test_permutation_of_palindrome.py
+++ 1_arrays_and_strings/permutation_or_palindrome/test_permutation_of_palindrome.py
@@ -7,18 +7,13 @@ import permutation_or_palindrome_pythonic
 
 
 class TestPermutationOfPalindrome(unittest.TestCase):
-    def setUp(self):
-        self.data = [
-            ('Tact Coa', True),
-            ('t a c t coapapa', True),
-            ('Tact Coax', False),
-            ('asdfadfadf', False)
-        ]
+    def setUp(self) -> None:
+        self.data = [("Tact Coa", True), ("t a c t coapapa", True), ("Tact Coax", False), ("asdfadfadf", False)]
 
-    def test_is_permutation_of_palindrome(self):
+    def test_is_permutation_of_palindrome(self) -> None:
         for string, expected_result in self.data:
             self.assertEqual(expected_result, permutation_of_palindrome.is_permutation_of_palindrome(string))
 
-    def test_is_permutation_of_palindrome_pythonic(self):
+    def test_is_permutation_of_palindrome_pythonic(self) -> None:
         for string, expected_result in self.data:
             self.assertEqual(expected_result, permutation_or_palindrome_pythonic.is_permutation_of_palindrome(string))
