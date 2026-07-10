8b718e605e52af2926d8761357ea1d4c856c8f28
rubenhortas <rubenhortas@gmail.com>
2026-07-10 11:20:49 +0200

    style: ruff

--- 1_arrays_and_strings/rotate_matrix/test_rotate_matrix.py
+++ 1_arrays_and_strings/rotate_matrix/test_rotate_matrix.py
@@ -1,87 +1,67 @@
 #!/usr/bin/env python3
 import unittest
 
-import rotate_matrix
 import rotate_matrix_alternative
 import rotate_matrix_double_swap
 import rotate_matrix_pythonic
 import rotate_matrix_swap_index_by_index
 from utils import print_matrix
 
+import rotate_matrix
+
 
 def _print_matrices(matrix: list, rotated_matrix: list, expected_result: list) -> None:
-    print('Matrix:')
+    print("Matrix:")
     print_matrix(matrix)
 
-    print('Rotated matrix:')
+    print("Rotated matrix:")
     print_matrix(rotated_matrix)
 
-    print('Expected result:')
+    print("Expected result:")
     print_matrix(expected_result)
 
 
 class TestRotateMatrix(unittest.TestCase):
-
-    def setUp(self):
+    def setUp(self) -> None:
         self.data = [
-            ([
-                 ['o', 'o', 'o'],
-                 ['*', '*', '*'],
-                 ['x', 'x', 'x']
-             ],
-             [
-                 ['x', '*', 'o'],
-                 ['x', '*', 'o'],
-                 ['x', '*', 'o']
-             ]),
-            ([
-                 ['00', '01', '02'],
-                 ['10', '11', '12'],
-                 ['20', '21', '22']
-             ],
-             [
-                 ['20', '10', '00'],
-                 ['21', '11', '01'],
-                 ['22', '12', '02'],
-             ]),
-            ([
-                 ['a', 'a', 'a'],
-                 ['b', 'b', 'b'],
-                 ['c', 'c', 'c']
-             ],
-             [
-                 ['c', 'b', 'a'],
-                 ['c', 'b', 'a'],
-                 ['c', 'b', 'a']
-             ])
+            ([["o", "o", "o"], ["*", "*", "*"], ["x", "x", "x"]], [["x", "*", "o"], ["x", "*", "o"], ["x", "*", "o"]]),
+            (
+                [["00", "01", "02"], ["10", "11", "12"], ["20", "21", "22"]],
+                [
+                    ["20", "10", "00"],
+                    ["21", "11", "01"],
+                    ["22", "12", "02"],
+                ],
+            ),
+            ([["a", "a", "a"], ["b", "b", "b"], ["c", "c", "c"]], [["c", "b", "a"], ["c", "b", "a"], ["c", "b", "a"]]),
         ]
 
-    def test_rotate_matrix(self):
+    def test_rotate_matrix(self) -> None:
         for matrix, expected_result in self.data:
             rotated_matrix = rotate_matrix.get_rotated_matrix(matrix)
             # _print_matrices(matrix, rotated_matrix, expected_result)
             self.assertEqual(expected_result, rotated_matrix)
 
-    def test_rotate_matrix_pythonic(self):
+    def test_rotate_matrix_pythonic(self) -> None:
         for matrix, expected_result in self.data:
             rotated_matrix = rotate_matrix_pythonic.get_rotated_matrix(matrix)
             # _print_matrices(matrix, rotated_matrix, expected_result)
             self.assertEqual(expected_result, rotated_matrix)
 
-    def test_rotate_matrix_alternative(self):
+    def test_rotate_matrix_alternative(self) -> None:
         for matrix, expected_result in self.data:
             rotated_matrix = rotate_matrix_alternative.get_rotated_matrix(matrix)
             # _print_matrices(matrix, rotated_matrix, expected_result)
             self.assertEqual(expected_result, rotated_matrix)
 
-    def test_rotate_matrix_swap_index_by_index(self):
+    def test_rotate_matrix_swap_index_by_index(self) -> None:
         for matrix, expected_result in self.data:
             # original_matrix = copy.deepcopy(matrix)
             rotated_matrix = rotate_matrix_swap_index_by_index.get_rotated_matrix(matrix)
             # _print_matrices(original_matrix, rotated_matrix, expected_result)
             self.assertEqual(expected_result, rotated_matrix)
 
-    def test_rotate_matrix_double_swap(self):
+    def test_rotate_matrix_double_swap(self) -> None:
         for matrix, expected_result in self.data:
             # original_matrix = copy.deepcopy(matrix)
             rotated_matrix = rotate_matrix_double_swap.get_rotated_matrix(matrix)
