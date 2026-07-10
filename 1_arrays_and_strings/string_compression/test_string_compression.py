4d56b8014e2a4840f121ec852471206689c2f111
rubenhortas <rubenhortas@gmail.com>
2026-07-10 11:33:56 +0200

    style: ruff

--- 1_arrays_and_strings/string_compression/test_string_compression.py
+++ 1_arrays_and_strings/string_compression/test_string_compression.py
@@ -2,22 +2,19 @@
 
 import unittest
 
-import string_compression
 import string_compression_pythonic
+
+import string_compression
 
 
 class TestStringCompression(unittest.TestCase):
-    def setUp(self):
-        self.data = [
-            ('aabcccccaaa', 'a2b1c5a3'),
-            ('bcda', 'bcda'),
-            ('aaa', 'a3')
-        ]
+    def setUp(self) -> None:
+        self.data = [("aabcccccaaa", "a2b1c5a3"), ("bcda", "bcda"), ("aaa", "a3")]
 
-    def test_string_compression(self):
+    def test_string_compression(self) -> None:
         for string, expected_result in self.data:
             self.assertEqual(expected_result, string_compression.get_compressed_string(string))
 
-    def test_string_compression_pythonic(self):
+    def test_string_compression_pythonic(self) -> None:
         for string, expected_result in self.data:
             self.assertEqual(expected_result, string_compression_pythonic.get_compressed_string(string))
